af7a7a531de85f82211e427185aeec1140e81364
SeanKi <ykish72@gmail.com>
2023-08-19 16:45:19 +0900

    Updates

--- assets/swipe-back-legacy-c5264526.js
+++ assets/swipe-back-legacy-c5264526.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-f3293ef5.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-38475d25.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
